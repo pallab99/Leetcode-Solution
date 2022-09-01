f92c78f21332da5a72ef489ae008419b59882093
pallab99 <37453678+pallab99@users.noreply.github.com>
2022-09-01 20:55:52 +0600

    Create 	Find Resultant Array After Removing Anagrams.cpp

---
+++ Find Resultant Array After Removing Anagrams.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long Get_hash(string s)
+    {
+        long long hash=1;
+
+        for(auto it:s)
+        {
+            hash+=hash*(it-'a');
+        }
+
+        return hash;
+    }
+    vector<string> removeAnagrams(vector<string>& words) {
+        stack<long long>st;
+        vector<string> res;
+        for(auto it:words)
+        {
+            long long hash=Get_hash(it);
+            if(!st.empty() and st.top()==hash)
+            continue;
+
+            st.push(hash);
+            res.push_back(it);
+
+        }
+
+        return res;
+    }
+};
