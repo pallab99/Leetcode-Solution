d7da21f6694a5142b17aeae698fff4bc3fbd9a15
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-09-07 18:50:38 +0600

    Time: 4 ms (29.72%), Space: 6.2 MB (15.90%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -4,15 +4,26 @@ public:
        
           vector<int>ans;
         int cnt=0;
-        for(int i=1;i<=n;i++)
+        int sq=sqrt(n);
+        for(int i=1;i<=sq;i++)
         {
             if(n%i==0)
+               {
+                if(i*i==n)
                 {
-                cnt++;
+                    cnt++;
                     ans.push_back(i);
                 }
+                else 
+                {
+                    cnt+=2;
+                    ans.push_back(i);
+                    ans.push_back(n/i);
+                }
+                
+                }
         }
-        
+        sort(ans.begin(),ans.end());
         if(cnt<k)
             return -1;
         return ans[k-1];
