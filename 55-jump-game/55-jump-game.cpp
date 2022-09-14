ac7b28d0d1dbee8045910aac60f44e74d6f1e128
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-09-14 22:02:57 +0600

    Time: 179 ms (26.05%), Space: 48.4 MB (77.79%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal=nums.size()-1;
+        int curr_pos=0;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            if(i+nums[i]>=goal)
+                goal=i;
+        }
+        
+        return goal<=0;
+    }
+};
