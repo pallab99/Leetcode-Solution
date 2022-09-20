692bd9c65e4d693b951f1bc394e7cf812f5c415b
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-09-20 12:35:42 +0600

    Time: 26 ms (37.86%), Space: 15.9 MB (56.84%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,51 +1,50 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int ele1=-1,ele2=-1,vote1=0,vote2=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-             if(ele1==nums[i])
-                vote1++;
-                else if(ele2==nums[i])
-                vote2++;
-          else   if(vote1==0)
-            {
-                ele1=nums[i];
-                vote1=1;
-            }
-            else if(vote2==0)
-            {
-                ele2=nums[i];
-                vote2=1;
-            }
-            else 
-            {
-               
-               
-                    vote1--;
-                    vote2--;
-                }
-            
-        }
-        int cnt1=0,cnt2=0;
+  vector<int> majorityElement(vector<int> &nums)
+  {
+    int ele1 = -1, ele2 = -1, vote1 = 0, vote2 = 0;
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+      if (ele1 == nums[i])
+        vote1++;
+      else if (ele2 == nums[i])
+        vote2++;
+      else if (vote1 == 0)
+      {
+        ele1 = nums[i];
+        vote1 = 1;
+      }
+      else if (vote2 == 0)
+      {
+        ele2 = nums[i];
+        vote2 = 1;
+      }
+      else
+      {
 
-        for(int i=0;i<n;i++)
-        {
-            if(ele1==nums[i])
-            cnt1++;
-            else if(ele2==nums[i])
-            cnt2++;
-        }
-
-        vector<int>ans;
-
-        if(cnt1>n/3)
-        ans.push_back(ele1);
-        if(cnt2>n/3)
-        ans.push_back(ele2);
-
-        return ans;
+        vote1--;
+        vote2--;
+      }
     }
-};
+    int cnt1 = 0, cnt2 = 0;
 
+    for (int i = 0; i < n; i++)
+    {
+      if (ele1 == nums[i])
+        cnt1++;
+      else if (ele2 == nums[i])
+        cnt2++;
+    }
+
+    vector<int> ans;
+
+    if (cnt1 > n / 3)
+      ans.push_back(ele1);
+    if (cnt2 > n / 3)
+      ans.push_back(ele2);
+
+    return ans;
+  }
+};
