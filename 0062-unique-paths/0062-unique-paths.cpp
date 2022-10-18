6188ded72e1e1aa106ec745b2384c439846a47f3
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-10-18 23:43:50 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (98.66%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[101][101];
+int recur(int m, int n, int i = 0, int j = 0)
+{
+    if (i >= m || j >= n)
+        return 0;
+    if (i == m - 1 && j == n - 1)
+        return 1;
+    if (dp[i][j])
+        return dp[i][j];
+    return dp[i][j] = recur(m, n, i + 1, j) + recur(m, n, i, j + 1);
+}
+int uniquePaths(int m, int n)
+{
+    return recur(m, n);
+}
+};
