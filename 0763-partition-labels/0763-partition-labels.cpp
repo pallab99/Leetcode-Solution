1250f7ddf9c1f3ce5d662d09c4a07da81f9bda86
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-10-22 00:18:13 +0600

    Time: 7 ms (62.36%), Space: 6.7 MB (69.62%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        map<char,int>mp;
-    for(int i=0;i<s.size();i++)
-    {
-        mp[s[i]]=i;
-    }
-    //for(auto it:mp)
-   // cout<<it.first<<" "<<it.second<<endl;
+        map<char, int> mp;
+    for (int i = 0; i < s.size(); i++)
+        mp[s[i]] = i;
 
-    int res=0;
-    int x=0;
-    vector<int>ans;
-    for(int i=0;i<s.size();i++)
+    int res = 0;
+    int x = 0;
+    vector<int> ans;
+    for (int i = 0; i < s.size(); i++)
     {
-        res=max(res,mp[s[i]]);
-        //debug(res);
-        if(i==res)
+        res = max(res, mp[s[i]]);
+        if (i == res)
         {
-            if(x==0)
+            if (x == 0)
             {
-                ans.push_back(res+1);
-                x=res+1;
+                ans.push_back(res + 1);
+                x = res + 1;
             }
-            else 
+            else
             {
-           //debug(res);
-           // debug(x);
-            ans.push_back(res-x+1);
-            x=res+1;
-           // debug(x);
-            }
 
+                ans.push_back(res - x + 1);
+                x = res + 1;
+            }
         }
-
     }
     return ans;
     }
