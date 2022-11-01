571c064f9b9e7c2d71e7e93a63742f9a0bdb7106
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-11-01 22:32:31 +0600

    Time: 44 ms (48.30%), Space: 5.7 MB (89.22%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-          if (n <= 0)
-        return false;
+           return n > 0 && 1162261467 % n == 0;
 
-    if (n % 3 == 0)
-        return isPowerOfThree(n / 3);
-
-    return n==1;
     }
 };
